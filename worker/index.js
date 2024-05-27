6ad5adf664ac2ad0c26df6d2562ac833dd73064e
Giovanni Troisi <giovanni@tiledesk.com>
2024-05-27 11:29:17 +0200

    updated worker/index.js

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const trainJobworker = require('@tiledesk/tiledesk-train-jobworker')
+
+if (process.env.TILELLM_ROLE === 'train') {
+    const trainJobworker = require('@tiledesk/tiledesk-train-jobworker')
+} else {
+    console.log("Worker non parte!!!!!!!")
+}
 
 app.get('/', (req, res) => {
     console.log("Tiledesk Trainer Worker Container works!");
+    if (trainJobworker) {
+        console.log("(Test Log) trainJobworker: ", trainJobworker);
+    }
     res.status(200).send("Tiledesk Trainer Worker Container works!");
 })
 
