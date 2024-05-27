34f4112f24f3a234788a65a8ef5b414edc253b30
Giovanni Troisi <giovanni@tiledesk.com>
2024-05-27 10:09:55 +0200

    added worker route

--- worker/index.js
+++ worker/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const trainJobworker = require('@tiledesk/tiledesk-train-jobworker')
 
 app.get('/', (req, res) => {
-    console.log("Tiledesk Trainer Worker works!");
-    res.status(200).send("Tiledesk Trainer Worker works!");
+    console.log("Tiledesk Trainer Worker Container works!");
+    res.status(200).send("Tiledesk Trainer Worker Container works!");
 })
 
 app.listen(3009, () => {
-    console.log("app listening on port 3009")
+    console.log("Tiledesk Trainer Worker Container listening on port 3009")
 })
