e44b2bc67867168abfaf55f8de9a6a6748e17c78
Gabriele Panico <gabriele.panico@gmail.com>
2024-05-30 12:51:21 +0200

    deploy changed

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # npm version patch
-version="0.1.18-rc12"
+version="0.1.18"
 echo "version $version"
 
 if [ "$version" != "" ]; then
     git add .
     git commit -m "Created a new version"
+    git push remote master --tags
     git tag -a "$version" -m "`git log -1 --format=%s`"
     echo "Created a new tag, $version"
-    git push remote --tags
 fi
